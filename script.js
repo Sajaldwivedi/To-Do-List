9c8d0a65c9bf3ff06f6d3872ae68d86d9e169723
Sajaldwivedi <127932657+Sajaldwivedi@users.noreply.github.com>
2025-02-14 22:52:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ const newtask = document.getElementById('putbox');
 const submitbutton = document.getElementsByClassName('submitbutton')[0];
 const activitylist = document.getElementById('activitylist');
 const completedlist = document.getElementById('completedlist');
+
 submitbutton.addEventListener('click', function() {
+    if (newtask.value.trim() !== "") {
         const li = document.createElement('li');
         li.textContent = newtask.value;
         li.addEventListener('click', function() {
@@ -10,4 +12,26 @@ submitbutton.addEventListener('click', function() {
         });
         activitylist.appendChild(li);
         newtask.value = "";
+    }
 });
+
+function moveToCompletedList(task) {
+    completedlist.appendChild(task);
+    task.removeEventListener('click', function() {
+        moveToCompletedList(task);
+    });
+    task.addEventListener('click', function() {
+        moveToToDoList(task);
+    });
+}
+
+function moveToToDoList(task) {
+    task.style.textDecoration = 'none';
+    activitylist.appendChild(task);
+    task.removeEventListener('click', function() {
+        moveToToDoList(task);
+    });
+    task.addEventListener('click', function() {
+        moveToCompletedList(task);
+    });
+}
