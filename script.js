9615aa959559d6780129ce1f59d1682194b371ff
Sajaldwivedi <127932657+Sajaldwivedi@users.noreply.github.com>
2025-02-14 22:48:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,12 @@ const newtask = document.getElementById('putbox');
 const submitbutton = document.getElementsByClassName('submitbutton')[0];
 const activitylist = document.getElementById('activitylist');
 const completedlist = document.getElementById('completedlist');
-submitbutton.addEventListener('click',funtion()
-{
-   const li=document.createElement('li');
-   li.textContent=newtask.value;
-})
+submitbutton.addEventListener('click', function() {
+        const li = document.createElement('li');
+        li.textContent = newtask.value;
+        li.addEventListener('click', function() {
+            moveToCompletedList(li);
+        });
+        activitylist.appendChild(li);
+        newtask.value = "";
+});
